fad8844414d024b7709dcd56ce8bc52b0c7c45b1
Asia <asialuk98@gmail.com>
2018-05-06 13:30:08 +0200

    Add page class

---
+++ src/frame_allocation_sym/Page.java
@@ -0,0 +1,27 @@
+package frame_allocation_sym;
+
+public class Page {
+	
+	private int pageNumeber;
+	private int callsNumber;
+	
+	
+	
+	public int getPageNumeber() {
+		return pageNumeber;
+	}
+
+	public void setPageNumeber(int pageNumeber) {
+		this.pageNumeber = pageNumeber;
+	}
+
+	public int getCallsNumber() {
+		return callsNumber;
+	}
+
+	public void setCallsNumber(int callsNumber) {
+		this.callsNumber = callsNumber;
+	}
+	
+	
+}
